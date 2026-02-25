a828b3c39eaec7113d0c5ab9e56e8700ba241076
Giovanna Xavier <giovannacostaxavier@gmail.com>
2026-02-25 19:45:44 +0000

    Testes do código

--- EXERCICIOS DE PSEUDOCODIGO CONVERTIDOS EM C/Exercicio 04/main.c
+++ EXERCICIOS DE PSEUDOCODIGO CONVERTIDOS EM C/Exercicio 04/main.c
@@ -1,35 +1,40 @@
-
 #include <stdio.h>
 
-int main(){
-    int teste1,teste2,teste3;
+int main() {
+    int teste1 = -1, teste2 = -1, teste3 = -1;  // inicializa com valor inválido
     float classFinal;
-    printf("Digite a nota do primeiro teste: ");
-    scanf("%d",&teste1);
-    
-    while(teste1 <0 || teste1 >20){
-        printf("Nota inválida, digite uma nota de 0 a 20: ");
-        scanf("%d",&teste1);
+
+    while (teste1 < 0 || teste1 > 20) {
+        printf("Digite a nota do primeiro teste: ");
+        scanf("%d", &teste1);
+        if (teste1 < 0 || teste1 > 20) {
+            printf("Nota inválida, a nota tem que estar entre 0 e 20.\n");
+        }
     }
-    printf("Digite a nota do segundo teste: ");
-    scanf("%d",&teste2);
-     while(teste2 <0 || teste2 >20){
-        printf("Nota inválida, digite uma nota de 0 a 20: ");
-        scanf("%d",&teste2);
+
+    while (teste2 < 0 || teste2 > 20) {
+        printf("Digite a nota do segundo teste: ");
+        scanf("%d", &teste2);
+        if (teste2 < 0 || teste2 > 20) {
+            printf("Nota inválida, a nota tem que estar entre 0 e 20.\n");
+        }
     }
-    printf("Digite a nota do terceiro teste :");
-    scanf("%d",&teste3);
-     while(teste3 <0 || teste3 >20){
-        printf("Nota inválida, digite uma nota de 0 a 20 :");
-        scanf("%d",&teste3);
+
+    while (teste3 < 0 || teste3 > 20) {
+        printf("Digite a nota do terceiro teste: ");
+        scanf("%d", &teste3);
+        if (teste3 < 0 || teste3 > 20) {
+            printf("Nota inválida, a nota tem que estar entre 0 e 20.\n");
+        }
     }
-    classFinal=teste1*0.2+teste2*0.5+teste3*0.3;
-    if(classFinal>=10){
-        printf("APROVADO");
-    }else{
-        printf("REPROVADO");
+
+    classFinal = teste1 * 0.2 + teste2 * 0.5 + teste3 * 0.3;
+
+    if (classFinal >= 10) {
+        printf("APROVADO\n");
+    } else {
+        printf("REPROVADO\n");
     }
-    
 
     return 0;
 }
